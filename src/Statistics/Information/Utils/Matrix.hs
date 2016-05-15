f0cd0775fcb2999518de65c8d603792364a2ea52
Eli Sennesh <eligottlieb@gmail.com>
2016-05-15 00:56:34 -0400

    Utils: added rowExtend to Utils.Matrix
    Added a function rowExtend to Utils.Matrix that should copy a matrix's contents
    into an arbitrarily large number of new rows, in such a way that the actual
    information content won't be affected.
    
    Testing Done: None at all!  Bad committer!
    
    Signed-off-by: Eli Sennesh <eligottlieb@gmail.com>

--- src/Statistics/Information/Utils/Matrix.hs
+++ src/Statistics/Information/Utils/Matrix.hs
@@ -1,10 +1,18 @@
 module Statistics.Information.Utils.Matrix where
 
 import Data.Matrix
-import Data.Vector
+import qualified Data.Vector as Vector
+import Statistics.Information.Utils.List
 
-rows :: Matrix a -> [Vector a]
+rows :: Matrix a -> [Vector.Vector a]
 rows xs = [getRow i xs | i <- [1..nrows xs]]
 
-columns :: Matrix a -> [Vector a]
+columns :: Matrix a -> [Vector.Vector a]
 columns xs = [getCol i xs | i <- [1..ncols xs]]
+
+rowExtend :: Matrix a -> Int -> Matrix a
+rowExtend xs n =
+  remRows <-> Prelude.foldr (<->) xs (repeatN (completions - 1) xs) where
+    completions = n `quot` nrows xs
+    remRows = fromLists [Vector.toList $ getRow i xs |
+                         i <- [1..n `rem` nrows xs]]
